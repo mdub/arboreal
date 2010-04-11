03c1696546d5430cabc9ad5f77159a4174e1583f
Mike Williams <mdub@dogbiscuit.org>
2010-04-11 20:34:45 +1000

    Seems to work nicely with activerecord-3.0.0.beta2.

--- lib/arboreal/instance_methods.rb
+++ lib/arboreal/instance_methods.rb
@@ -1,3 +1,5 @@
+require "active_support/core_ext/string/filters"
+
 module Arboreal
   module InstanceMethods
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,7 +41,7 @@ DB_CONFIGS = {
   }
 }
 
-test_adapter = (ENV["ARBOREAL_TEST_ADAPTER"] || "sqlite3")
+test_adapter = (ENV["AR_ADAPTER"] || "sqlite3")
 test_db_config = DB_CONFIGS[test_adapter].merge(:adapter => test_adapter)
 
 Spec::Runner.configure do |config|
