c48447eff41ac2399f5d541eab7b7ddd71328c88
Randy Coulman <rcoulman@gmail.com>
2015-05-04 11:48:04 -0700

    Remove Rails 2 references; bump verison to 0.3.0

--- lib/arboreal/active_record_extensions.rb
+++ lib/arboreal/active_record_extensions.rb
@@ -2,12 +2,10 @@ require 'active_record'
 
 module Arboreal
   module ActiveRecordExtensions
-
     # Declares that this ActiveRecord::Base model has a tree-like structure.
     def acts_arboreal
-
-      belongs_to :parent, :class_name => self.name
-      has_many   :children, :class_name => self.name, :foreign_key => :parent_id
+      belongs_to :parent, class_name: self.name
+      has_many   :children, class_name: self.name, foreign_key: :parent_id
 
       extend Arboreal::ClassMethods
       include Arboreal::InstanceMethods
@@ -21,20 +19,8 @@ module Arboreal
       before_save :detect_ancestry_change
       after_save  :apply_ancestry_change_to_descendants
 
-      case ActiveRecord::VERSION::MAJOR
-
-      when 3
-        scope :roots, where(:parent_id => nil)
-
-      when 2
-        named_scope :roots, {
-          :conditions => ["parent_id IS NULL"]
-        }
-
-      end
-
+      scope :roots, lambda { where(parent_id: nil) }
     end
-
   end
 end
 
